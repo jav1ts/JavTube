ee85c6abdaddffbefafa2c222a9c0115b1021f9b
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-08-29 16:33:37 -0400

    added code to update state in the App component when the searchbar form is submitted

--- javtube/src/apis/youtube.js
+++ javtube/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyD6tgfqS-95ovgAxAHhTSO5HsYP0lBqTWs';
+const KEY = 'AIzaSyCkDAYRTqsykNh9CIGlmmERtjnnEQkTje8';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
