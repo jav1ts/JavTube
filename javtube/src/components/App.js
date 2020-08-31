f7ff80e476eddd926e33f84faf438cb0c2c6349a
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-08-31 01:34:40 -0400

    added VideoList and VideoItem components

--- javtube/src/components/App.js
+++ javtube/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
     state = { videos : [] };
@@ -20,7 +21,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
